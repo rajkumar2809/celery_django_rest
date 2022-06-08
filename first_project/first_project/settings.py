071f2157c03901425b64d15d85d1d6cc00e29b59
unknown <rajeshkumarpattnayak@gmail.com>
2022-06-08 17:08:14 +0530

    Added logs , static , media folders and added the code on the tasks.py file that will save the data on the database

--- first_project/first_project/settings.py
+++ first_project/first_project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -114,19 +115,20 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Kolkata'
 
 USE_I18N = True
 
 USE_L10N = True
 
-USE_TZ = True
+USE_TZ = False
 
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
--- first_project/middleapp/tasks.py
+++ first_project/middleapp/tasks.py
@@ -1,24 +1,23 @@
 from celery import shared_task
 import json
-import requests
 from django.http import HttpResponse
-import pymongo
+from middleapp.models import *
+from rest_framework import status
 
 @shared_task
 def send_data_through_celery(data):
     data1 = json.loads(data)
     print("Data 1 from tasks",data1)
     try:
-        #myclient = pymongo.MongoClient("mongodb://165.22.210.193:27017/")
-        #mydb = myclient["MIDDLE_APPLICATION"]
-        #mycol = mydb["middleapp_mdl_string"]
-
-        url = 'http://165.22.210.193:8005/mdl_post_string/'
-        request_type = "POST"
-        data2 =  {'uid':data1['uid'] , 'name': data1['name']}
+        # url = 'http://165.22.210.193:8005/mdl_post_string/'
+        # request_type = "POST"
+        # data2 =  {'uid':data1['uid'] , 'name': data1['name']}
+        saveto_db = mdl_string (uid = data1['uid'] , name = data1['name'] )
+        saveto_db.save()
         #x = mycol.insert_one(data2)
         # x = mycol.insert_many(data2)
-        _response = requests.request(request_type, url, data=data2)
-        return HttpResponse("Response after saved on database {} " .format(_response))
+        # _response = requests.request(request_type, url, data=data2)
+        # return Response(request.data , status = status.HTTP_201_CREATED)
+        return HttpResponse("status {} " .format(status.HTTP_201_CREATED))
     except Exception as e:
         return HttpResponse("Exception from tasks {} " .format(e))
